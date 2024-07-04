00c4296529f3388ae8868746315a0bc04db451f0
Nazar Pavlenko <kozhov2@gmail.com>
2024-07-04 21:56:01 +0300

    add

--- contacts/script.js
+++ contacts/script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const phoneInput = document.getElementById('phone');
+    const form = document.getElementById('callbackForm');
+    const message = document.getElementById('message');
 
     phoneInput.addEventListener('input', function() {
         const maxLength = 9;
@@ -11,6 +13,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
         if (!/^\d*$/.test(value)) {
             phoneInput.value = value.replace(/\D/g, '');
+        }
+    });
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const phone = phoneInput.value;
+
+        if (phone.length === 9) {
+            message.textContent = "Дякую за ваше звернення, наш консультант зв'яжеться з вами найближчим часом.";
+            message.style.color = '#0044cc'; // Блакитний колір
+        } else {
+            message.textContent = "Ваш номер телефону був введений некоректно, будь ласка, повторіть спробу.";
+            message.style.color = '#ff0000'; // Червоний колір для помилки
+            phoneInput.value = '';
         }
     });
 });
